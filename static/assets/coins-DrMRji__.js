4ed9dd7eb685d16ef9a02a33e9641acf43f30b40
Swarali <forchatgpt2425@gmail.com>
2026-09-25 23:53:58 +0530

    Add static file serving to merge frontend and backend on Render; copy built frontend to static/ directory

---
+++ static/assets/coins-DrMRji__.js
@@ -0,0 +1,1 @@
+import{l as e}from"./Card-DOD6GKEn.js";var t={name:`coins`,size:24,node:[[`path`,{d:`M13.744 17.736a6 6 0 1 1-7.48-7.48`,key:`bq4yh3`}],[`path`,{d:`M15 6h1v4`,key:`11y1tn`}],[`path`,{d:`m6.134 14.768.866-.5 2 3.464`,key:`17snzx`}],[`circle`,{cx:`16`,cy:`8`,r:`6`,key:`14bfc9`}]]};t.node;var n=e(t);export{n as t};
---
+++ static/assets/database-Cx64k5JH.js
@@ -0,0 +1,1 @@
+import{l as e}from"./Card-DOD6GKEn.js";var t={name:`database`,size:24,node:[[`ellipse`,{cx:`12`,cy:`5`,rx:`9`,ry:`3`,key:`msslwz`}],[`path`,{d:`M3 5V19A9 3 0 0 0 21 19V5`,key:`1wlel7`}],[`path`,{d:`M3 12A9 3 0 0 0 21 12`,key:`mv7ke4`}]]};t.node;var n=e(t);export{n as t};
